a9ae2058ee3a243355e889890d58071e5ede7f98
IvanCarrera <ivanvilac@gmail.com>
2025-07-15 08:35:37 +0200

    ArrayList

--- src/main/java/es/cic/curso2025/proy005/TelefonoController.java
+++ src/main/java/es/cic/curso2025/proy005/TelefonoController.java
@@ -1,6 +1,10 @@
 package es.cic.curso2025.proy005;
 
+import java.util.List;
+import java.util.ArrayList; // Importación añadida
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +12,44 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/telefono")
 public class TelefonoController {
 
+    private int contador = 0;
+
     @GetMapping
-    public String saludo() {
-        return "Hola Mundo";
+    public List<Telefono> get() {
+
+        contador++;
+
+        List<Telefono> resultado = new ArrayList<>();
+
+        Telefono telefono1 = new Telefono();
+        telefono1.setId(1L);
+        telefono1.setNumero("123456789");
+        telefono1.setTipoContrato(1);
+        telefono1.setTitular("Juan Perez");
+
+        resultado.add(telefono1);
+
+        Telefono telefono2 = new Telefono();
+        telefono2.setId(2L); // Id diferente
+        telefono2.setNumero("987654321");
+        telefono2.setTipoContrato(2);
+        telefono2.setTitular("Maria Lopez");
+
+        resultado.add(telefono2);
+
+        return resultado;
+    }
+
+    @GetMapping("/{id}")
+    public List<Telefono> get(@PathVariable long id) {
+        
+
+        Telefono telefono1 = new Telefono();
+        telefono1.setId(1L);
+        telefono1.setNumero("123456789");
+        telefono1.setTipoContrato(1);
+        telefono1.setTitular("Juan Perez");
+
+        return List.of(telefono1); 
     }
 }
--- src/test/java/es/cic/curso2025/proy005/Proy005ApplicationTests.java
+++ src/test/java/es/cic/curso2025/proy005/Proy005ApplicationTests.java
@@ -1,5 +1,7 @@
 package es.cic.curso2025.proy005;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,4 +12,15 @@ class Proy005ApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	void testLista(){
+		ArrayList miLista = new ArrayList();
+		miLista.add("Hola");
+
+		String resultado = (String) miLista.get(0);
+
+		ArrayList<String> miLista2 = new ArrayList<>();
+		miLista2.add("Hola Mundo");
+		String resultado2 = miLista2.get(0);
+	}
 }
